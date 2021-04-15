d5af7b7047d529344ee9091e473ff392803f9c0b
arnaud boscq <itsmycar@air-de-its.home>
2021-04-15 12:55:05 +0100

    Le composant gère son apparition et disparition (opendaley, closedelay)

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -2,14 +2,32 @@ import React, { useState } from "react";
 import "./Tooltip.css";
 
 const Tooltip = (props) => {
+  let timeout;
+  const [active, setActive] = useState(false);
+
+  const showTip = () => {
+    timeout = setTimeout(() => {
+      setActive(true);
+    }, props.opendelay);
+  };
+
+  const hideTip = () => {
+    clearInterval(timeout);
+    timeout = setTimeout(() => {
+      setActive(false);
+    }, props.closedelay);
+  };
+
   return (
     <div
       className="Tooltip-Wrapper"
       // When to show the tooltip
+      onMouseEnter={showTip}
+      onMouseLeave={hideTip}
     >
       {/* Wrapping */}
       {props.children}
-      {(
+      {active && (
         <div className={`Tooltip-Tip ${props.direction || "top"}`}>
           {/* Content */}
           {props.content}
