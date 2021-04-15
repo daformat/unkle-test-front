dbdd70ce89a8d0cb562b23c94dcf8faecc7e61cd
arnaud boscq <itsmycar@air-de-its.home>
2021-04-15 13:27:47 +0100

    Le composant gère ses dimensions minimales et maximales (minwidth, maxwidth)

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
         </div>
       </div>
       <hr/>
-      <h3>II) Le composant gère son apparition et disparition (opendaley, closedelay)</h3>
+      <h3>II) Le composant gère son apparition et disparition (opendelay, closedelay)</h3>
       <br/>
       <div className="container main">
         <div className="row">
@@ -117,6 +117,61 @@ function App() {
                 direction="right"
                 opendelay="2000"
                 closedelay="2000"
+              >
+                <span className="example-emoji" role="img" aria-label="cowboy emoji">
+                  🤠
+                </span>
+              </Tooltip>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr/>
+      <h3>III) Le composant gère ses dimensions minimales et maximales (minwidth, maxwidth)</h3>
+      <br/>
+      <div className="container main">
+        <div className="row">
+          <div className="col-sm-3">
+            <div className="example-wrapper">
+              <Tooltip 
+                content="Yee-haw! Yee-haw! Yee-haw! Yee-haw!" 
+                direction="left"
+              >
+                <span className="example-emoji" role="img" aria-label="cowboy emoji">
+                  🤠
+                </span>
+              </Tooltip>
+            </div>
+          </div>
+          <div className="col-sm-3">
+            <div className="example-wrapper">
+              <Tooltip 
+                content="Yee-haw!" 
+                direction="right"
+              >
+                <span className="example-emoji" role="img" aria-label="cowboy emoji">
+                  🤠
+                </span>
+              </Tooltip>
+            </div>
+          </div>
+          <div className="col-sm-3">
+            <div className="example-wrapper">
+              <Tooltip 
+                content="Yee-haw! Yee-haw! Yee-haw! Yee-haw!Yee-haw! Yee-haw! Yee-haw! Yee-haw!" 
+                direction="top"
+              >
+                <span className="example-emoji" role="img" aria-label="cowboy emoji">
+                  🤠
+                </span>
+              </Tooltip>
+            </div>
+          </div>
+          <div className="col-sm-3">
+            <div className="example-wrapper">
+              <Tooltip 
+                content="Yee-haw!Yee-haw!Yee-haw! Yee-haw! Yee-haw!Yee-haw! Yee-haw! Yee-haw! Yee-haw!" 
+                direction="bottom"
               >
                 <span className="example-emoji" role="img" aria-label="cowboy emoji">
                   🤠
